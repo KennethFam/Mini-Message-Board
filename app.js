573de997149670a1e9447aee048da588676a804a
KennethFam <147673498+KennethFam@users.noreply.github.com>
2025-05-13 08:01:05 -0700

    added error 404

--- app.js
+++ app.js
@@ -16,9 +16,16 @@ messageRouter = require("./routes/message");
 app.use("/", indexRouter);
 app.use("/new", newRouter);
 app.use("/message", messageRouter);
+app.use(["/:file", "/new/:file", "/message/:file"], (req, res, next) => {
+    next(err);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is currently running on port ${PORT}.`);
+});
+
+app.use((err, req, res, next) => {
+    res.status(404).send("Error 404: Page not found.");
 });
   
   
