a7ecb89b30cb8e1227bde5a3c72022dae553aee4
KennethFam <147673498+KennethFam@users.noreply.github.com>
2025-05-19 21:06:00 -0700

    fixed bug where page was not found

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,6 +4,6 @@ module.exports = {
     get: async (req, res) => {
         // remember to await async functions
         const messages = await db.getAllMessages();
-        res.render("../views/index", {title: "Mini Message Board", messages: messages});
+        res.render("index", {title: "Mini Message Board", messages: messages});
     }
 }
